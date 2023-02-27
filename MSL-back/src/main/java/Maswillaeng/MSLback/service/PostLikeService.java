183238fc98375ae1f438fe4c7173aec794e88ffb
YunByungil <dbsquddlfz@naver.com>
2023-02-27 20:12:57 +0900

    feat: PostLikeController, 좋아요 취소 기능 구현
    PostLikeRepository -> findByPostIdAndUserId 메서드로 해당 게시글에 해당 유저가 좋아요를 누른 상태인지 아닌지 조회한다. 없으면 취소 완료. (컬럼 삭제)

--- MSL-back/src/main/java/Maswillaeng/MSLback/service/PostLikeService.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/service/PostLikeService.java
@@ -40,9 +40,6 @@ public class PostLikeService {
     }
 
 
-    /*
-    TODO: PostLikeRepository -> findByPostIdAndUserId 생성
-     */
     public void unlikePost(Long postId, Long userId) {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new IllegalStateException("해당 게시글이 존재하지 않습니다."));
@@ -50,14 +47,14 @@ public class PostLikeService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalStateException("해당 회원이 존재하지 않습니다."));
 
-//        PostLike postLike = postLikeRepository.findByPostIdAndUserId(postId, userId)
-//                .orElseThrow(() -> new IllegalStateException("좋아요 취소됨"));
+        PostLike postLike = postLikeRepository.findByPostIdAndUserId(postId, userId)
+                .orElseThrow(() -> new IllegalStateException("해당 게시글에 좋아요를 누르지 않았습니다."));
 
 //        PostLike postLike = PostLike.builder()
 //                .post(post)
 //                .user(user)
 //                .build();
 
-//        postLikeRepository.delete(postLike);
+        postLikeRepository.delete(postLike);
     }
 }
