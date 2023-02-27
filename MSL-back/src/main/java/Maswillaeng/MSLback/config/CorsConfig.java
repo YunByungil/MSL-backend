3e0bc276554c5592f85143ac7461a5877e2b4a4c
YunByungil <dbsquddlfz@naver.com>
2023-02-28 00:33:42 +0900

    refactor: CorsConfig 경로 수정

--- MSL-back/src/main/java/Maswillaeng/MSLback/config/CorsConfig.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
         config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/**", config);
         return source;
     }
 
