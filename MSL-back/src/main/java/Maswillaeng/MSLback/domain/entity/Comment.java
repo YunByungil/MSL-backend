3388705584e3231155f933aff39b7e1c52219790
YunByungil <dbsquddlfz@naver.com>
2023-03-03 23:41:10 +0900

    refactor: Comment Entity, 대댓글 cascade 속성 추가
    부모 댓글이 삭제될 때, 자식 댓글도 삭제해야 되기 때문에 cascade를 추가했음

--- MSL-back/src/main/java/Maswillaeng/MSLback/domain/entity/Comment.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/domain/entity/Comment.java
@@ -35,7 +35,7 @@ public class Comment extends BaseEntity{
     @JoinColumn(name = "parent_id")
     private Comment parent;
 
-    @OneToMany(mappedBy = "parent")
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.REMOVE)
     private List<Comment> child = new ArrayList<>();
 
     /**
