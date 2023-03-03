ed522e71fa630f8970c2da2337fac362704a5a05
YunByungil <dbsquddlfz@naver.com>
2023-03-03 23:41:37 +0900

    refactor: CorsCinfig, addAllowedOrigin 주소 추가

--- MSL-back/src/main/java/Maswillaeng/MSLback/config/CorsConfig.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/config/CorsConfig.java
@@ -17,6 +17,8 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:8080");
         config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
