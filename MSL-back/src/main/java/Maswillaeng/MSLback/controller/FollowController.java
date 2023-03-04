3c4e775d02a56326f077c3d7f7a7d08bfcbe1c59
YunByungil <dbsquddlfz@naver.com>
2023-03-04 20:48:54 +0900

    feat: FollowController, 팔로우 취소 기능 완료

--- MSL-back/src/main/java/Maswillaeng/MSLback/controller/FollowController.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/controller/FollowController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,18 @@ public class FollowController {
         Long myId = Long.parseLong(authentication.getName());
         followService.addFollow(myId, userId);
         return ResponseEntity.ok().body("팔로우 완료!");
+    }
+
+    /**
+     * 팔로우 취소 기능
+     * @param userId
+     * @param authentication
+     * @return
+     */
+    @DeleteMapping("/api/{userId}/follow")
+    public ResponseEntity unFollow(@PathVariable("userId") Long userId, Authentication authentication) {
+        Long myId = Long.parseLong(authentication.getName());
+        followService.unFollow(myId, userId);
+        return ResponseEntity.ok().body("팔로우 취소 완료");
     }
 }
