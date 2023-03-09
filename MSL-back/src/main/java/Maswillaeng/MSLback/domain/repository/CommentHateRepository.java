bfc0672c7018c7165c8aa4cad526f472210ae8c3
YunByungil <dbsquddlfz@naver.com>
2023-03-09 18:23:40 +0900

    feat: CommentHateRepository 구현

---
+++ MSL-back/src/main/java/Maswillaeng/MSLback/domain/repository/CommentHateRepository.java
@@ -0,0 +1,8 @@
+package Maswillaeng.MSLback.domain.repository;
+
+import Maswillaeng.MSLback.domain.entity.CommentHate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentHateRepository extends JpaRepository<CommentHate, Long> {
+
+}
