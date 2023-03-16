d526f41d77334c84b0f67fb8549f9826e03cade3
YunByungil <dbsquddlfz@naver.com>
2023-03-16 15:09:06 +0900

    feat: HashTag,Tag Repository 생성

---
+++ MSL-back/src/main/java/Maswillaeng/MSLback/domain/repository/HashTagRepository.java
@@ -0,0 +1,6 @@
+package Maswillaeng.MSLback.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashTagRepository extends JpaRepository<HashTagRepository, Long> {
+}
