7d900b8477a7830f9dbfa481c35fa9430eb231ab
YunByungil <dbsquddlfz@naver.com>
2023-03-21 23:01:46 +0900

    feat: 게시글 전체 조회 HashTag 추가

--- MSL-back/src/main/java/Maswillaeng/MSLback/dto/post/reponse/PostListResponseDto.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/dto/post/reponse/PostListResponseDto.java
@@ -1,11 +1,15 @@
 package Maswillaeng.MSLback.dto.post.reponse;
 
 import Maswillaeng.MSLback.domain.entity.Post;
+import Maswillaeng.MSLback.domain.entity.Tag;
 import Maswillaeng.MSLback.domain.enums.Category;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class PostListResponseDto {
@@ -22,6 +26,7 @@ public class PostListResponseDto {
     private Long likeCount;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createAt;
+    private List<String> hashTag = new ArrayList<>();
 
     public PostListResponseDto(Post post, int commentCount, int likeCount) {
         this.postId = post.getId();
@@ -35,5 +40,8 @@ public class PostListResponseDto {
         this.commentCount = (long)commentCount;
         this.likeCount = (long)likeCount;
         this.createAt = post.getCreateAt();
+        this.hashTag = post.getHashTag().stream()
+                .map(t -> t.getTag().getName())
+                .collect(Collectors.toList());
     }
 }
