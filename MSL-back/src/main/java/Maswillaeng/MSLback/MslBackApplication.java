8404ac7a5213b82c76fe2725ae1b6f9e7abbbd6f
YunByungil <dbsquddlfz@naver.com>
2023-04-06 18:10:21 +0900

    build: ignore 테스트

--- MSL-back/src/main/java/Maswillaeng/MSLback/MslBackApplication.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/MslBackApplication.java
@@ -2,12 +2,18 @@ package Maswillaeng.MSLback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class MslBackApplication {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(MslBackApplication.class, args);
+		new SpringApplicationBuilder(MslBackApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 }
