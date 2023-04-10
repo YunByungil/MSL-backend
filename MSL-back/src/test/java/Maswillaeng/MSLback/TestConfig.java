63775625d6dadb5bfd4ded69d2b9c411f8d5fabc
YunByungil <dbsquddlfz@naver.com>
2023-04-10 23:36:06 +0900

    fix: build/libs 이름 수정

--- MSL-back/src/test/java/Maswillaeng/MSLback/TestConfig.java
+++ MSL-back/src/test/java/Maswillaeng/MSLback/TestConfig.java
@@ -1,20 +1,20 @@
-package Maswillaeng.MSLback;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-@TestConfiguration
-public class TestConfig {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    @Bean
-    public JPAQueryFactory queryFactory() {
-        return new JPAQueryFactory(em);
-    }
-}
+//package Maswillaeng.MSLback;
+//
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import org.springframework.boot.test.context.TestConfiguration;
+//import org.springframework.context.annotation.Bean;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//
+//@TestConfiguration
+//public class TestConfig {
+//
+//    @PersistenceContext
+//    private EntityManager em;
+//
+//    @Bean
+//    public JPAQueryFactory queryFactory() {
+//        return new JPAQueryFactory(em);
+//    }
+//}
--- MSL-back/src/test/java/Maswillaeng/MSLback/domain/repository/CommentLikeRepositoryTest.java
+++ MSL-back/src/test/java/Maswillaeng/MSLback/domain/repository/CommentLikeRepositoryTest.java
@@ -1,46 +1,46 @@
-package Maswillaeng.MSLback.domain.repository;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@DataJpaTest
-class CommentLikeRepositoryTest {
-
-    @Autowired
-    private CommentLikeRepository commentLikeRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private CommentRepository commentRepository;
-
-    @BeforeEach
-
-
-    @DisplayName("commentLikeRepository가 null인지 확인")
-    @Test
-    void isNotNull() {
-        // given
-        Assertions.assertThat(commentLikeRepository).isNotNull();
-        // when
-
-        // then
-    }
-
-    @DisplayName("댓글 좋아요 등록")
-    @Test
-    void likeComment() {
-        // given
-        userRepository.findById(1L);
-
-        // when
-
-        // then
-    }
-}
+//package Maswillaeng.MSLback.domain.repository;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@DataJpaTest
+//class CommentLikeRepositoryTest {
+//
+//    @Autowired
+//    private CommentLikeRepository commentLikeRepository;
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//    @Autowired
+//    private CommentRepository commentRepository;
+//
+//    @BeforeEach
+//
+//
+//    @DisplayName("commentLikeRepository가 null인지 확인")
+//    @Test
+//    void isNotNull() {
+//        // given
+//        Assertions.assertThat(commentLikeRepository).isNotNull();
+//        // when
+//
+//        // then
+//    }
+//
+//    @DisplayName("댓글 좋아요 등록")
+//    @Test
+//    void likeComment() {
+//        // given
+//        userRepository.findById(1L);
+//
+//        // when
+//
+//        // then
+//    }
+//}
